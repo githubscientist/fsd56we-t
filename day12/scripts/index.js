8be05222ff28fb52516397bba6d2aea5bb5bf785
githubscientist <sathish.datascientist@gmail.com>
2024-03-23 12:40:41 +0530

    loops, lists dom

--- day12/scripts/index.js
+++ day12/scripts/index.js
@@ -1,46 +1,63 @@
-// create a reference for container div
-const container = document.getElementById('container');
+// const btn4 = document.getElementById('btn4');
+const btn4 = document.querySelector('#btn4'); // select by id
 
-// console.log(container);
+btn4.style.backgroundColor = 'red';
+btn4.style.color = 'white';
 
-// create a new h1 element
-const h1 = document.createElement('h1');
+const btn1 = document.querySelector('button'); // select by tag
 
-h1.textContent = 'Hello World!';
+btn1.style.backgroundColor = 'blue';
+btn1.style.color = 'white';
 
-// console.log(document.body);
+// console.log(btn1);
 
-// console.log(h1);
-container.appendChild(h1);
+const buttons = document.querySelectorAll('button');
+
+for (let i = 0; i < buttons.length; i++){
+    if(buttons[i].style.backgroundColor !== 'blue' && buttons[i].style.backgroundColor !== 'red'){
+        buttons[i].style.backgroundColor = 'green';
+        buttons[i].style.color = 'white';
+    }
+}
+
+// console.log(buttons);
+
+/*
+    1. Create a unordered list with the list items content of your favorite foods.
+
+    favoriteFoods = ['Pizza', 'Burger', 'Pasta', 'Fried Chicken', 'Ice Cream', 'Donut', 'Cake', 'Fries', 'Shawarma', 'Tacos']
+
+*/
+
+const container = document.querySelector('#container');
+
+favoriteFoods = ['Pizza', 'Burger', 'Pasta', 'Fried Chicken', 'Ice Cream', 'Donut', 'Cake', 'Fries', 'Shawarma', 'Briyani'];
+
+// create an unordered list
+const foodsList = document.createElement('ul');
+
+favoriteFoods.forEach(food => {
+    const foodItem = document.createElement('li');
+    foodItem.innerText = food;
+    foodsList.appendChild(foodItem);
+});
+
+container.appendChild(foodsList);
 
 /*
     Exercise: 
 
-    1. Create a new paragraph element and set its text content to "This is a paragraph". Append it to the body.
-
-    2. Create a anchor element and set its content to "visit guvi.in" and set its href to "https://www.guvi.in". Append it to the body. [hint: use setAttribute('href', 'https://www.guvi.in') to set the attribute].
+    From the above created list of favorite foods, change the text color of the odd list items to red and the even list items to blue.
 */
 
-// Create a new paragraph element
+// const foodItems = document.querySelectorAll('li');
 
-const paragraph = document.createElement('p');
+// // console.log(foodItems);
 
-// set its text content to "This is a paragraph".
-paragraph.textContent = 'This is a paragraph';
-
-// Append it to the body.
-container.appendChild(paragraph);
-
-// Create a anchor element
-const anchor = document.createElement('a');
-
-// set its content to "visit guvi.in" 
-anchor.textContent = 'visit guvi.in';
-
-// set its href to "https://www.guvi.in".[hint: use setAttribute('href', 'https://www.guvi.in') to set the attribute]
-anchor.setAttribute('href', 'https://www.guvi.in');
-
-anchor.setAttribute('target' , '_blank');
-
-// Append it to the body.
-container.appendChild(anchor);
+// for (let index = 0; index < foodItems.length; index++){
+//     if (index % 2 == 0) {
+//         foodItems[index].style.color = 'red';
+//     } else {
+//         foodItems[index].style.color = 'blue';
+//     }
+// }
--- readme.md
+++ readme.md
@@ -6,9 +6,22 @@
 [x] setAttribute  
 [x] getElementById  
 [x] querySelector vs querySelectorAll  
-[] innerHTML vs innerText  
-[x] appendChild vs append
+[x] innerHTML vs innerText
 
-### Pre-read:
+textContent:
 
-https://github.com/rvsp/html-CSS/
+- The textContent property sets or returns the text content of the specified node, and all its descendants.
+
+innerText:
+
+- The innerText property sets or returns the text content of the specified node, and all its descendants.
+
+innerHTML:
+
+- The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+
+[x] appendChild vs append ### Pre-read: https://github.com/rvsp/html-CSS/
+
+```
+
+```
