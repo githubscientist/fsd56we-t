53db6247e779c75a549baedcbb46c26269525c14
githubscientist <sathish.datascientist@gmail.com>
2024-03-24 12:21:29 +0530

    localstorage

--- day13/scripts/index.js
+++ day13/scripts/index.js
@@ -1,20 +1,35 @@
-// console.log(window);
+// window.localStorage.setItem('name', 'John Doe');
+// window.localStorage.setItem('age', 30);
+// window.localStorage.setItem('isMarried', false);
 
-console.log(window.location); // all the information about the current URL
 
-// console.log(window.location.href); // URL
 
-// console.log(window.location.hostname); // domain name
+// console.log(window.localStorage);
+// console.log(window.localStorage.getItem('name'));
 
-// console.log(window.location.origin); // protocol + hostname
+// window.localStorage.removeItem('name');
+// window.localStorage.removeItem('age');
+// window.localStorage.clear();
 
-// console.log(window.location.protocol); // http or https
+// window.localStorage.setItem('age', 45);
 
-// console.log(window.location.port); // port number
+// const recentSearches = [
+//     {
+//         title: 'laptops',
+//         similarSearches: ['laptops', 'laptops for sale', 'laptops under $500']
+//     },
+//     {
+//         title: 'smartphones',
+//         similarSearches: ['smartphones', 'smartphones for sale', 'smartphones under $500']
+//     },
+//     {
+//         title: 'tablets',
+//         similarSearches: ['tablets', 'tablets for sale', 'tablets under $500']
+//     }
+// ]
 
-// redirecting to a different url
-// window.location.href = '/login.html';
+// localStorage.setItem('recentSearches', JSON.stringify(recentSearches));
 
-function redirect(path) {
-    window.location.href = path;
-}
+const searches = JSON.parse(localStorage.getItem('recentSearches'));
+
+console.log(searches[2]);
