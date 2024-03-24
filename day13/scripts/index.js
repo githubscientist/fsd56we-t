a5ba5fc5d73b21d73ea9c1739591af8e640cf069
githubscientist <sathish.datascientist@gmail.com>
2024-03-24 13:04:47 +0530

    event listeners -keyboard event

--- day13/scripts/index.js
+++ day13/scripts/index.js
@@ -1,12 +1,23 @@
-// let name = prompt('What is your name?');
+// const loginButton = document.getElementById('loginButton');
 
-// console.log('Hello, ' + name + '!');
+// loginButton.addEventListener('dblclick', () => {
+//     window.location.href = '/day13/pages/login.html';
+// })
+let ctrlPressed = false;
 
-// alert('The script is running!');
+document.addEventListener('keydown', (event) => {
+    // console.log('key presssed:', event.key);
 
-// let confirmation = confirm('Do you want to delete this file?');
+    if (event.key === 'Control') {
+        ctrlPressed = true;
+    } else if (event.key === 's' && ctrlPressed) {
+        event.preventDefault();
+        console.log('Ctrl + S pressed');
+    }
+})
 
-// if (confirmation) {
-//     alert('File deleted!');
-// } 
-
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'Control') {
+        ctrlPressed = false;
+    }
+});
